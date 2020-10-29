c9006bbe8edd341f6819c0391943d45d04dd2c32
Asier Cidon <acidonpe@redhat.com>
2020-10-29 01:46:12 +0100

    added back url correctly

--- src/infrastructure/postJump.tsx
+++ src/infrastructure/postJump.tsx
@@ -6,8 +6,8 @@ export const sentJump = async (
     data: Jump
   ): Promise<ResponseBack> => {
 
-    //const url: string = "http://golang-demo-git-myapp.apps.68.sandbox1017.opentlc.com/jump"
-    const url: string = "http://localhost:8442/jump"
+    const url: string = "http://golang-demo-git-myapp.apps.68.sandbox1017.opentlc.com/jump"
+    // const url: string = "http://localhost:8442/jump"
     const options = { 
       headers: { 
         'Content-Type': "application/json", 
