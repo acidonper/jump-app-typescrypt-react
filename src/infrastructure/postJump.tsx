4aada8d8d7e8c91293955b031839ba36ee49db1c
Asier Cidon <acidonpe@redhat.com>
2020-11-03 18:11:53 +0100

    Added backend url

--- src/infrastructure/postJump.tsx
+++ src/infrastructure/postJump.tsx
@@ -5,15 +5,13 @@ import { Response as ResponseBack } from '../domain/Response';
 
 
 export const sentJump = async (
-    data: Jump, calls: number, callsInterval: number
+    url: string, data: Jump, calls: number, callsInterval: number
   ): Promise<ResponseBack> => {
 
     const timeout = async (ms: number) => {
       return new Promise(resolve => setTimeout(resolve, ms*1000));
     }
 
-    const url: string = "http://golang-demo-git-myapp.apps.68.sandbox1017.opentlc.com/jump"
-    // const url: string = "http://localhost:8442/jump"
     const options = { 
       headers: { 
         'Content-Type': "application/json", 
