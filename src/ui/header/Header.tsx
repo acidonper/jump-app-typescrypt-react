8bd2681d50c5a887eb2848581223574f6c31b80b
AsierCidon <asier.cidon@gmail.com>
2021-01-16 23:12:14 +0100

    Added environment header

--- src/ui/header/Header.tsx
+++ src/ui/header/Header.tsx
@@ -14,7 +14,7 @@ export const Header: React.FunctionComponent<Props> = () => {
         <a href='/'>
           <Logo size='xxl'></Logo>
         </a>
-        <h1>Jump App v2.0</h1>
+        <h1>Jump App v2.0 DEV</h1>
       </div>
     </>
   );
